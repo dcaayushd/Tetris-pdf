587de61d48b156583755190cc9313fe9280ba456
dcaayushd <aayushdc123@yahoo.com>
2025-01-28 15:46:06 +0545

    Constants (I 0 T- Piece) Added

---
+++ lib/utils/constants.dart
@@ -0,0 +1,19 @@
+const int boardWidth = 10;
+const int boardHeight = 20;
+
+const List<List<List<int>>> pieces = [
+  // I-piece
+  [
+    [1, 1, 1, 1]
+  ],
+  // O-piece
+  [
+    [1, 1],
+    [1, 1]
+  ],
+  // T-piece
+  [
+    [0, 1, 0],
+    [1, 1, 1]
+  ],
+];
