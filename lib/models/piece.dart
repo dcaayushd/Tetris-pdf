ae8f1c9add9b77f2ef48a21394375dd6603875c2
dcaayushd <aayushdc123@yahoo.com>
2025-01-28 15:48:55 +0545

    Piece Models Added

---
+++ lib/models/piece.dart
@@ -0,0 +1,27 @@
+class Piece {
+  late List<List<int>> shape;
+  late int rotationState;
+
+  Piece(List<List<int>> shape) {
+    this.shape = shape;
+    rotationState = 0;
+  }
+
+  void rotate() {
+    rotationState = (rotationState + 1) % 4;
+    shape = _rotateShape(shape);
+  }
+
+  List<List<int>> _rotateShape(List<List<int>> shape) {
+    final int rows = shape.length;
+    final int cols = shape[0].length;
+    final List<List<int>> newShape = List.generate(cols, (i) => List.filled(rows, 0));
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        newShape[j][rows - 1 - i] = shape[i][j];
+      }
+    }
+    return newShape;
+  }
+}
